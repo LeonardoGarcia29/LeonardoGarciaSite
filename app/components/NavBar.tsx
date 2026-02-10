b840834d6213fd9eb56ceb29ded0d3d75f805e93
Leonardo Garcia <garciaherreraleonardo29@gmail.com>
2026-02-09 21:42:14 -0800

    Added Navbar, and simple home page layout

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+  return (
+    <nav className="flex bg-gray-800 h-16 justify-evenly items-center">
+      <div>
+        Home
+      </div>
+      <div>
+        Transcript
+      </div>
+      <div>
+        Projects
+      </div>
+    </nav>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,71 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Leonardo Garcia Personal Site.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Requirements:<br/>
-                  - A picture of yourself<br/>
-                  - Video of self-introduction (minimum of 1 minute and no more than 1.5 minutes)<br/>
-                  - Link to transcript information in an HTML TABLE format<br/>
-                  - A drop-down box with at least 2 choices (point to 2 separate school or internship projects that
-                    you have done with descriptions in separate HTML page/file)<br/>
-                  - Google Map with a marker on your home address on your main page<br/>
-                  - Readme file<br/>{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-             <br/> Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex">
+      <div className="bg-blue-500 flex-[3]">
+        information about me
+      </div>
+      <div className="bg-red-500 flex-[2]">
+        picture of me
+      </div>
     </div>
   );
 }
